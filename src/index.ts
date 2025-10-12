d54692dffac87067a322e39764261b9ac74927c8
Ahmed Cvrcak <ahmed@karriera.de>
2025-10-12 17:09:12 +0200

    CORS added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import imageRoutes from "./routes/images";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/images", imageRoutes);
 
 const PORT = process.env.PORT || 3000;
